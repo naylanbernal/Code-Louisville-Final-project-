fa6fd3c78af84e72629245bc6fc04889c75c7365
naylanbernal <naylan.work@gmail.com>
2023-11-28 20:31:36 -0500

    . improved CSS . Change the background . button that leads to the beginning of the page . working on the the dogs description using js

--- aplication.js
+++ aplication.js
@@ -1,20 +1,14 @@
 const ulList = document.querySelector(".ul-list")
 const linkList = document.querySelectorAll(".link-list")
-console.log(linkList)
-const searchInput = document.querySelector(".search-input")
+const list = document.querySelectorAll(".list")
+const searchIcon = document.querySelector(".search-icon")
+const searchlittleBox = document.querySelector(".search-littleBox")
+const divBreed = document.querySelector(".breed")
+let breedDescription = document.querySelector(".breed-description")
+
+const objects_array = []
 
 
-
-searchInput.addEventListener("mouseover", ()=>{
-    linkList.forEach(item =>{
-        ulList.style.display = "block"
-        item.style.display = "block"
-        ulList.style.zIndex = 20
-    })
-})
-ulList.addEventListener("mouseleave", ()=>{
-    ulList.style.display = "none"
-})
 
 
 
@@ -23,13 +17,56 @@ const apiLink = 'https://dogapi.dog/api/v2/breeds'
 fetch(apiLink)
     .then(res => res.json())
     .then(info => {
-        //console.log(info);
-        //console.log(info.data[3].attributes.name)
         info.data.forEach(element => {
-            //console.log(element.attributes.name)
-            //console.log(element.attributes.description)
-            
+            let obj = {
+                name: element.attributes.name,
+                descrip: element.attributes.description
+            }
+            objects_array.push(obj)
         });
+    })
+
+
+
+
+
+searchlittleBox.addEventListener("mouseover", ()=>{
+    linkList.forEach(item =>{
+        item.style.display = "block"
+        searchIcon.style.width = "30px"
+        searchIcon.style.height = "30px"
+        ulList.style.display = "block"
+        ulList.style.zIndex = "100"
+    })
+})
+ulList.addEventListener("mouseleave", ()=>{
+    ulList.style.display = "none"
+    searchIcon.style.width = "50px"
+    searchIcon.style.height = "50px"
+})
+
+
+
+    
+
+
+function linkToDescription(){
+    let liName = ""
+    setTimeout(()=>{
+        
+        list.forEach(item =>{
+            item.addEventListener("click", ()=>{
+                liName = item.textContent;
+            })
+        
+        }, 1000)
         
     })
+    console.log(liName);
+}
+linkToDescription()
+
+
+
+
 
